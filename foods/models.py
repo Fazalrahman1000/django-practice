dbe9c6da7c06bc1bb1eb4de8b2c416cfcceb1a10
Fazalrahman1000 <fazalrahmanwaqar3@gmail.com>
2025-12-07 08:00:06 +0430

    django practice

--- foods/models.py
+++ foods/models.py
@@ -6,6 +6,7 @@ class FoodItem(models.Model):
     item_name = models.CharField(max_length=100)
     description = models.TextField()
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500, default="https://cdn-icons-png.freepik.com/256/11680/11680860.png")
 
     def __str__(self):
         return f"{self.item_name} - ${self.item_price}"
