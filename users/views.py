fa627ca55d06c38a50f1de3f2257d49b6e25d56f
Fazalrahman1000 <fazalrahmanwaqar3@gmail.com>
2025-12-13 21:48:10 +0430

    django practice

--- users/views.py
+++ users/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterUser
 from django.contrib import messages
 # Create your views here.
 
 def create_user(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegisterUser(request.POST)
         if form.is_valid():
             user = form.save()
             username = form.cleaned_data.get('username')
             message = messages.success(request, f"Welcome {username} to our website, your account is created")
             return redirect('foods:index')
     else:
-        form = UserCreationForm()
+        form = RegisterUser()
     return render(request, 'users/create_user.html', {'form':form})
